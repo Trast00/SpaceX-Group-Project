b4e124fc0f2fdfae80ad2e439af6ccf6bfa841f8
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-01-31 11:34:00 +0500

    Create a action and reducer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rocketReducer from './rockets/rocketReducer';
+import missionReducer from './missions/missionReducer';
 
 export default configureStore({
   reducer: {
     rocketReducer,
+    mission: missionReducer.reducer,
   },
 });
